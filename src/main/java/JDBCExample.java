8204d11602badb7247144566c53871d883ea708d
lukir <radluki@gmail.com>
2016-12-17 18:40:14 +0100

    Initial commit

---
+++ src/main/java/JDBCExample.java
@@ -0,0 +1,63 @@
+//STEP 1. Import required packages
+import java.sql.*;
+// -cp  /home/lukir/IdeaProjects/FirstProject:/usr/local/mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar
+public class JDBCExample {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "luki";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating database...");
+            stmt = conn.createStatement();
+
+            // Testing User
+            User.statement = stmt;
+            User u1 = new User(105, "name", "pass", 7.9, 3);
+
+            System.out.println(u1.convertToString());
+            u1.insert();
+
+
+            // End testing User
+
+            String sql = "INSERT INTO tab VALUES (\"aaa\")";//"CREATE DATABASE STUDENTS";
+            stmt.executeUpdate(sql);
+            System.out.println("Database created successfully...");
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }//end main
+}//end JDBCExample
---
+++ src/main/java/User.java
@@ -0,0 +1,50 @@
+/**
+ * Created by lukir on 17.12.16.
+ */
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class User {
+    static String __table__ = "USERS";
+    public static Statement statement;
+    int ID;
+    String Name;
+    String Password;
+    double x;//position
+    double y;
+
+    public User(int ID, String Name, String Password, double x, double y){
+        this.ID = ID;
+        this.Name = Name;
+        this.Password = Password;
+        this.x = x;
+        this.y = y;
+        try {
+            this.statement.execute("CREATE TABLE IF NOT EXISTS USERS (ID INTEGER PRIMARY KEY, Name VARCHAR(40), Password VARCHAR(40), x FLOAT(8,4), y FLOAT(8,4));");
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+    }
+
+    String convertToString() {
+        return "'"+this.ID+"', '"+this.Name +"', '"+ this.Password + "', '"+this.x+ "', '"+this.y+"'";
+    }
+
+    void insert(){
+        try {
+            this.statement.execute("INSERT INTO " + this.__table__ + " VALUES(" + this.convertToString() + ");");
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+    }
+
+    public static User getUserFromDatabase(String name,String password){
+        User u = User.statement.executeQuery("SELECT * FROM "+User.__table__+" WHERE Name = "+name+", Password = "+password);
+        return u;
+    }
+
+    public static void main(String[] args) {
+        User u1 = new User(100, "name", "pass", 2, 3);
+        System.out.println(u1.convertToString());
+    }
+}
